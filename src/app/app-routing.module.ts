77b0f8c252806cf57ae9c310cbf4ffcdd9177ea9
V <maja5225@hotmail.com>
2020-06-29 16:41:18 +0200

    about, contact, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: "", component: HomePageComponent
+    },
+    {
+        path: "portfolio", component: PortfolioPageComponent
+    }, 
+    {
+        path: "about", component: AboutPageComponent
+    }, 
+    {
+        path: "contact", component: ContactPageComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    PortfolioPageComponent
+    PortfolioPageComponent,
+    AboutPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
