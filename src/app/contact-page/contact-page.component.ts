8dec7a923bdcf0d7ac78c2f0fd0e60a3d6155ab7
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2020-06-30 15:03:46 +0200

    contact page modification and some functionality

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-page',
@@ -6,6 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-page.component.scss']
 })
 export class ContactPageComponent implements OnInit {
+
+  contactInfo = new FormGroup({
+
+    firstName : new FormControl('',Validators.required),
+    lastName : new FormControl('',Validators.required),
+    email : new FormControl('',Validators.required),
+    phone : new FormControl(''),
+    subject : new FormControl('',Validators.required),
+    message : new FormControl('',Validators.required)
+
+  });
+  
+
+  modalTitle : string;
+  modalText : string;
+
+  submitInfo(){
+    if(this.contactInfo.valid){
+
+      this.modalTitle = 'Success';
+      this.modalText = 'Thank you for contacting us'
+      var formInfo = this.contactInfo.value;
+      console.log(formInfo);
+    }
+    else{
+
+      this.modalTitle = 'Required Fields missing :';
+      this.modalText = '';
+      var formInfo = this.contactInfo.value;
+
+
+      if(formInfo.firstName === ''){
+        this.modalText += '- First Name\n';
+      }
+      if(formInfo.lastName === ''){
+        this.modalText += '- Last Name\n';
+      }
+      if(formInfo.email === ''){
+        this.modalText += '- Email\n';
+      }
+      if(formInfo.subject === ''){
+        this.modalText += '- Subject\n';
+      }
+      if(formInfo.message === ''){
+        this.modalText += '- Message\n';
+      }
+
+    }
+  };
 
   constructor() { }
 
