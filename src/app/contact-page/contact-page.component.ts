cf9af8c8256e28d45462f5d4ba460584d60c371d
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2020-06-30 22:36:48 +0200

    added more Validation and visual changes

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -12,7 +12,7 @@ export class ContactPageComponent implements OnInit {
 
     firstName : new FormControl('',Validators.required),
     lastName : new FormControl('',Validators.required),
-    email : new FormControl('',Validators.required),
+    email : new FormControl('',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]),
     phone : new FormControl(''),
     subject : new FormControl('',Validators.required),
     message : new FormControl('',Validators.required)
@@ -22,21 +22,25 @@ export class ContactPageComponent implements OnInit {
 
   modalTitle : string;
   modalText : string;
+  validErr : boolean = false;
+  emailErr : boolean = false;
 
   submitInfo(){
     if(this.contactInfo.valid){
 
+      this.validErr = false;
+      this.emailErr = false;
       this.modalTitle = 'Success';
-      this.modalText = 'Thank you for contacting us'
+      this.modalText = 'Thank you for contacting us !'
       var formInfo = this.contactInfo.value;
       this.contactInfo.reset();
     }
     else{
-
-      this.modalTitle = 'Required Fields missing :';
+      
+      this.validErr = true;
+      this.modalTitle = 'Something went wrong';
       this.modalText = '';
       var formInfo = this.contactInfo.value;
-
 
       if(formInfo.firstName === '' || formInfo.firstName === null){
         this.modalText += '- First Name\n';
@@ -47,13 +51,18 @@ export class ContactPageComponent implements OnInit {
       if(formInfo.email == '' || formInfo.email === null){
         this.modalText += '- Email\n';
       }
+      else if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formInfo.email)){
+        this.emailErr = false;
+      }
+      else{
+        this.emailErr = true;
+      }
       if(formInfo.subject == '' || formInfo.subject === null){
         this.modalText += '- Subject\n';
       }
       if(formInfo.message == '' || formInfo.message === null){
         this.modalText += '- Message\n';
       }
-
     }
   };
 
